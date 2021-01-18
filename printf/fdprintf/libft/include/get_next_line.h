b9113d2fe4334a98aa3b83b619680c6074e7b412
AcensJJ <uentosacens@gmail.com>
2021-01-18 14:31:25 +0100

    Update get_next_line.h

--- printf/fdprintf/libft/include/get_next_line.h
+++ printf/fdprintf/libft/include/get_next_line.h
@@ -10,6 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE	4096
+#endif
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -21,7 +25,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# define BUFFER_SIZE	4096
 
 typedef struct			s_list_gnl
 {
